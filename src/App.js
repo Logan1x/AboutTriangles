e142891ccf33f9a105f53d5f5e1c0785e43ba470
logan1x <sharmakhushal78@gmail.com>
2021-08-09 11:19:10 +0530

    Removed Un-nessasery things

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 function App() {
   return (
     <div className="App">
+      {/* Start editing here */}
       <div className="w-full h-screen flex justify-center items-center">
         <h1 className="font-bold text-5xl text-indigo-600">Hello Tailwind <span className="bg-gray-100 p-2 hover:bg-transparent cursor-wait">CSS</span></h1>
       </div>
     </div>
   );
 }
+
 
 export default App;
